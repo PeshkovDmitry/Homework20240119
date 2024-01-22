f93f09f969c7aec5e642487ed276a5fe047b18f5
PeshkovDmitry <peshkov-dv@yandex.ru>
2024-01-22 14:01:06 +0300

    Доработал алгоритм поиска оптимальной точки

--- src/games/model/AI.java
+++ src/games/model/AI.java
@@ -1,7 +1,5 @@
 package games.model;
 
-import java.util.Random;
-
 import static games.resources.Config.*;
 
 public class AI {
@@ -13,26 +11,15 @@ public class AI {
     }
 
     public void turn() {
-        int x, y;
         FieldDot recommendedDot =  getRecommendedDot();
-        System.out.println(recommendedDot);
-        if (recommendedDot.isUndefined()) {
-            do {
-                x = new Random().nextInt(FIELD_SIZE);
-                y = new Random().nextInt(FIELD_SIZE);
-            } while (!field.isCellValid(x, y));
-        } else {
-            x = recommendedDot.getX();
-            y = recommendedDot.getY();
-        }
-        field.setDot(x, y, AI_DOT);
+        field.setDot(recommendedDot.getX(), recommendedDot.getY(), AI_DOT);
     }
 
     /**
      * Метод для получения наиболее оптимальной точки
      * @return Оптимальная точка
      */
-    public FieldDot getRecommendedDot() {
+    private FieldDot getRecommendedDot() {
         FieldDot recommendedDot;
         // Проверяем, есть ли точка, обеспечивающая выигрыш одним ходом
         recommendedDot = getWinnerDot(AI_DOT);
@@ -44,7 +31,14 @@ public class AI {
         if (!recommendedDot.isUndefined()) {
             return recommendedDot;
         }
-        return new FieldDot();
+        // Определяем наиболее выгодную точку
+        OptimumDot humanDot = getOptimumDotForChar(HUMAN_DOT);
+        OptimumDot aiDot = getOptimumDotForChar(AI_DOT);
+        if (aiDot.getRating() > humanDot.getRating()) {
+            return new FieldDot(aiDot.getX(), aiDot.getY());
+        } else {
+            return new FieldDot(humanDot.getX(), humanDot.getY());
+        }
     }
 
     /**
@@ -53,7 +47,7 @@ public class AI {
      * @return Точка игрового поля
      */
 
-    public FieldDot getWinnerDot(char dot) {
+    private FieldDot getWinnerDot(char dot) {
         for (int i = 0; i < FIELD_SIZE; i++) {
             for (int j = 0; j < FIELD_SIZE; j++) {
                 char[][] map = field.getMap();
@@ -67,4 +61,91 @@ public class AI {
         }
         return new FieldDot();
     }
+
+    /**
+     * Метод для подсчета наибольшего количества символов
+     * среди доступных для сборки линий в данной точке
+     * @param x Координата x
+     * @param y Координата y
+     * @return Максимум
+     */
+    private int checkLine(int x, int y, char dot) {
+
+        int result = -1;
+        char[][] map = field.getMap();
+        int count = 0;
+        for (int i = 0; i < FIELD_SIZE; i++) {
+            if (map[x][i] == dot) {
+                count++;
+            }
+            if (map[x][i] == EMPTY_DOT) {
+                map[x][i] = dot;
+            }
+        }
+        result = (field.checkWin(dot, map) && result < count) ? count : result;
+
+        map = field.getMap();
+        count = 0;
+        for (int i = 0; i < FIELD_SIZE; i++) {
+            if (map[i][y] == dot) {
+                count++;
+            }
+            if (map[i][y] == EMPTY_DOT) {
+                map[i][y] = dot;
+            }
+        }
+        result = (field.checkWin(dot, map) && result < count) ? count : result;
+
+        if (x == y) {
+            map = field.getMap();
+            count = 0;
+            for (int i = 0; i < FIELD_SIZE; i++) {
+                if (map[i][i] == dot) {
+                    count++;
+                }
+                if (map[i][i] == EMPTY_DOT) {
+                    map[i][i] = dot;
+                }
+            }
+            result = (field.checkWin(dot, map) && result < count) ? count : result;
+        }
+
+        if (x == FIELD_SIZE - 1 - y) {
+            map = field.getMap();
+            count = 0;
+            for (int i = 0; i < FIELD_SIZE; i++) {
+                if (map[i][FIELD_SIZE - 1 - y] == dot) {
+                    count++;
+                }
+                if (map[i][FIELD_SIZE - 1 - y] == EMPTY_DOT) {
+                    map[i][FIELD_SIZE - 1 - y] = dot;
+                }
+            }
+            result = (field.checkWin(dot, map) && result < count) ? count : result;
+        }
+        return result;
+    }
+
+    /**
+     * Метод для получения координат оптимальной точки
+     * @param dot Символ
+     * @return Координаты оптимума
+     */
+
+    private OptimumDot getOptimumDotForChar(char dot) {
+        int max = -1;
+        OptimumDot maxCoords = new OptimumDot();
+        char[][] map = field.getMap();
+        for (int i = 0; i < FIELD_SIZE; i++) {
+            for (int j = 0; j < FIELD_SIZE; j++) {
+                int current = checkLine(i, j, dot);
+                if (max < current) {
+                    max = current;
+                    maxCoords = new OptimumDot(i, j, max);
+                }
+            }
+        }
+        return maxCoords;
+    }
+
 }
